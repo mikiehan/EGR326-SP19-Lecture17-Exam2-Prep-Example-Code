3b1b3d9b6a5fb539b8a86d860aaffb23ade09952
Mi Kyung Han <mhan@calbaptist.edu>
2019-04-01 20:55:52 -0700

    updating ListOfLists

--- src/iterator/ListOfList.java
+++ src/iterator/ListOfList.java
@@ -2,36 +2,47 @@ package iterator;
 import java.util.Iterator;
 import java.util.List;
 
-public class ListOfList implements Iterator<String> {
+public class ListOfList {
     private List<List<String>> list;
-    private int outerIndex;
-    private int innerIndex;
 
     public ListOfList(List<List<String>> list){
         this.list = list;
-        outerIndex = 0;
-        innerIndex = 0;
     }
 
-    @Override
-    public boolean hasNext() {
-        if(outerIndex < list.size()){
-            List<String> innerList = list.get(outerIndex);
-            return innerIndex < innerList.size();
-        }else{
-            return false;
-        }
+    public Iterator<String> getIterator(){
+        return new MyIterator();
     }
 
-    @Override
-    public String next() {
-        List<String> innerList = list.get(outerIndex);
-        String str = innerList.get(innerIndex);
-        innerIndex++;
-        if(innerIndex == innerList.size()) {
-            outerIndex++;
+    //Inner class that implements Iterator interface
+    private class MyIterator implements Iterator<String> {
+        private int outerIndex;
+        private int innerIndex;
+
+        public MyIterator(){
+            outerIndex = 0;
             innerIndex = 0;
         }
-        return str;
+
+        @Override
+        public boolean hasNext() {
+            if (outerIndex < list.size()) {
+                List<String> innerList = list.get(outerIndex);
+                return innerIndex < innerList.size();
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public String next() {
+            List<String> innerList = list.get(outerIndex);
+            String str = innerList.get(innerIndex);
+            innerIndex++;
+            if (innerIndex == innerList.size()) {
+                outerIndex++;
+                innerIndex = 0;
+            }
+            return str;
+        }
     }
 }
